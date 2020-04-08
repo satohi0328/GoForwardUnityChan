4e105e9835be62864a44ca6af01a5390a0edfd11
satohi0328 <31176803+satohi0328@users.noreply.github.com>
2020-04-08 23:31:27 +0900

    Add GoForwardUnityChanプロジェクト 初回コミット
    Lesson7 2Dゲームアプリを作ろう

---
+++ Assets/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // 時間計測用の変数
+    private float delta = 0;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine) {
+            Destroy(gameObject);
+        }
+    }
+
+    // 衝突時
+    void OnCollisionEnter2D(Collision2D other) {
+        // 他キューブに衝突した場合、音声を再生
+        if (other.gameObject.tag == "CubeTag") {
+            GetComponent<AudioSource>().Play();
+        }
+
+        // 地面に衝突した場合、音声を再生
+        if (other.gameObject.name == "ground") {
+            GetComponent<AudioSource>().Play();
+
+        }
+    }
+
+}
